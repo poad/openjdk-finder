a8d05d8b5dfe5dd6f4fd8f87e95177da4c0c8470
Kenji Saito <ken-yo@mbr.nifty.com>
2020-04-12 13:20:52 +0900

    Fixed to not worked in local

--- web-client/src/components/Config.ts
+++ web-client/src/components/Config.ts
@@ -3,8 +3,8 @@ export interface Config {
 }
 
 const config: Config = {
-  endpoint: process.env.API_ENDPOINT
-    ? process.env.API_ENDPOINT
+  endpoint: process.env.NODE_ENV == 'development'
+    ? 'http://localhost:8089/'
     : 'https://openjdk-finder.herokuapp.com/'
 }
 
