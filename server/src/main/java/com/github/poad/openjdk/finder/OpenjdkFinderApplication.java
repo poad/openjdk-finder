db6f2ecde397716bc6c52506c25184a58303d69e
Kenji Saito <ken-yo@mbr.nifty.com>
2020-04-18 22:32:15 +0900

    Fix the CORS access. (#25)
    * Replace HTTP Client to axios
    
    * Allow all domain CORS.

--- server/src/main/java/com/github/poad/openjdk/finder/OpenjdkFinderApplication.java
+++ server/src/main/java/com/github/poad/openjdk/finder/OpenjdkFinderApplication.java
@@ -1,6 +1,8 @@
 package com.github.poad.openjdk.finder;
 
 import com.github.poad.openjdk.finder.config.Cors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -16,38 +18,42 @@ import java.util.Set;
 @EnableConfigurationProperties(Cors.class)
 public class OpenjdkFinderApplication {
 
-	private final Cors cors;
-	private final Set<String> paths = Set.of(
-			"/",
-			"/vendors",
-			"/versions",
-			"/architectures",
-			"/distributions",
-			"/os",
-			"/types",
-			"/bundles"
-	);
+    private final Cors cors;
+    private final Set<String> paths = Set.of(
+            "/",
+            "/vendors",
+            "/versions",
+            "/architectures",
+            "/distributions",
+            "/os",
+            "/types",
+            "/bundles"
+    );
+    private final Logger log = LogManager.getLogger(this.getClass());
 
-	OpenjdkFinderApplication(Cors cors) {
-		this.cors = cors;
-	}
+    OpenjdkFinderApplication(Cors cors) {
+        this.cors = cors;
+    }
 
-	public static void main(String... args) {
-		SpringApplication.run(OpenjdkFinderApplication.class, args);
-	}
+    public static void main(String... args) {
+        SpringApplication.run(OpenjdkFinderApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		var cors = this.cors;
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				cors.getOrigins().ifPresent(origins -> origins.forEach(origin -> {
-					paths.forEach(
-							path -> registry.addMapping(path).allowedMethods("GET", "HEAD", "OPTIONS").allowedOrigins(origin)
-					);
-				}));
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        var cors = this.cors;
+
+        cors.getOrigins().ifPresent(origin -> origin.forEach(log::info));
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                cors.getOrigins().ifPresent(origins ->
+                        paths.forEach(
+                                path -> registry.addMapping(path).allowedMethods("GET", "HEAD", "OPTIONS").allowedOrigins(origins.toArray(new String[0]))
+                        )
+                );
+            }
+        };
+    }
 }
